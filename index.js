5eeb6cddcacf5dc8e8f9c286ba82d3f601f328e8
Gabriel Pistilli <gabrielcarra.p@gmail.com>
2024-04-21 13:52:14 -0300

    despertador

--- index.js
+++ index.js
@@ -48,3 +48,36 @@ function watch() {
     document.getElementById("watch").innerText = twoDigits(hour) + ":" + twoDigits(min) + ":" + twoDigits(sec)
 }
 
+
+
+function iniciarDespertador(){
+    const timer = document.getElementById("timer")
+    const hora = document.getElementById("horas-value").value;
+    const minutos = document.getElementById("minutos-value").value;
+    const segundos = document.getElementById("segundos-value").value;
+    const somDespertador = new Audio("alarm_clock_5.mp3")
+
+    if(hora == "" && minutos == "" && segundos == ""){
+        alert("Não foi possível salvar o despertador, informe o horário corretamente.")
+    }
+    else{
+        alert("Despertador programado para: " + hora + ":" + minutos + ":" + segundos)
+        timer.innerText = `${hora} : ${minutos} : ${segundos}`
+    }
+
+    const interval = setInterval(function(){
+        const date = new Date();
+        const currentHour = date.getHours();
+        const currentMinute = date.getMinutes();
+        const currentSecond = date.getSeconds();
+
+        if(currentHour == hora && currentMinute == minutos && currentSecond == segundos){
+            somDespertador.play();
+            var visualizou = confirm("Despertador tocando!");
+            if(visualizou){
+                somDespertador.pause();
+                clearInterval(interval)
+            }
+        }
+    }, 1000)
+}
